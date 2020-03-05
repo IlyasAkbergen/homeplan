f8cca21f0fe3a0402502047e4cc5122024ba45dc
eldarkhasen <eldarkhasen@gmail.com>
2020-03-05 22:24:09 +0600

    Добавили Resources для Image,ApartmentComplex,Layout

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,23 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseSuccess($result, $message='', $code = 200)
+    {
+        return $this->response('success', $result, $message, $code);
+    }
+
+    public function responseFail($result, $message='', $code = 400)
+    {
+        return $this->response('fail', $result, $message, $code);
+    }
+
+    public function responseFailValidation($result)
+    {
+        return $this->responseFail($result, 'validation', 403);
+    }
+
+    protected function response($status, $result, $message, $code) {
+        return response()->json( [ 'status' => $status, 'message'=>$message, 'result' => $result ], $code);
+    }
 }
--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Layout;
 use Illuminate\Http\Request;
 
 class LayoutController extends Controller
 {
-    //
+    public function getLayoutsByComplex($id){
+
+    }
 }
