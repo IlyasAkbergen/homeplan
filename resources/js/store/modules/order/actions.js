3290211c8238be7d4a0431ffe50c8f19da973f6b
eldarkhasen <eldarkhasen@gmail.com>
2020-03-10 16:32:05 +0600

    Правки по deploy

--- resources/js/store/modules/order/actions.js
+++ resources/js/store/modules/order/actions.js
@@ -2,7 +2,7 @@ import { axiosInstance as axios } from '../../../../js/plugins/axios'
 
 export default {
     getOrderResult ({ commit }, data) {
-        return axios.post('/orders/', data)
+        return axios.post('/orders', data)
             .then((res) => {
                 commit('setOrderResult', res.data.result);
                 return res
