76671c404b505cae9f1d2d620ff3599c0adc2ff0
ilyasakbergen <ilijas9813@gmail.com>
2020-03-12 23:58:01 +0600

    design update

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -31,16 +31,4 @@
 
 <style lang="sass">
     @import "../assets/style/all.css"
-
-    /*.component-fade-enter-active, .component-fade-leave-active, .fade-enter-active, .fade-leave-active*/
-    /*    transition: opacity .3s ease*/
-    /*.component-fade-enter, .component-fade-leave-to, .fade-enter, .fade-leave-to*/
-    /*    opacity: 0*/
-    .slide-fade-enter-active
-        transition: all .2s ease
-    .slide-fade-leave-active
-        transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0)
-    .slide-fade-enter, .slide-fade-leave-to
-      transform: translateX(10px)
-      opacity: 0
 </style>
